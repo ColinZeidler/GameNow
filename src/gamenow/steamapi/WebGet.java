4a5549a29bffeb851f8c2e68a1565dbc8088ecf4
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-14 00:23:47 -0500

    added output function to print recieved response

--- src/gamenow/steamapi/WebGet.java
+++ src/gamenow/steamapi/WebGet.java
@@ -6,6 +6,7 @@
 package gamenow.steamapi;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
@@ -26,23 +27,27 @@ public class WebGet {
         model = gamenow.Model.getinstance();
     }
 
-    public void connect() {
+    public WebGet connect() {
         try {
             url = new URL("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key="
                     + model.getKey() + "&steamid=" + model.getUserID() + "&format=json");
         } catch (MalformedURLException ex) {
-//            Logger.getLogger(WebGet.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(WebGet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        return this;
     }
-//    InputStream is = url.openConnection().getInputStream();
-//
-//    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-//
-//    String line = null;
-//    while ((line  = reader.readLine()) != null) {
-//            System.out.println(line);
-//    }
-//
-//    reader.close ();
+    
+    public void output() throws IOException{
+        InputStream is = url.openConnection().getInputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+        String line = null;
+        while ((line  = reader.readLine()) != null) {
+                System.out.println(line);
+        }
+
+        reader.close();
+    }
 
 }
