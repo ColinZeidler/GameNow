92a195caf10893e26e744019d4fa805dc2b80ae9
Colin Zeidler <c.zeidler.wc@gmail.com>
2014-01-13 21:31:51 -0500

    classes for steam api integration added

---
+++ src/gamenow/steamapi/WebGet.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gamenow.steamapi;
+
+/**
+ *
+ * @author Colin
+ * Connects to the steam api and downloads that game data associated with the set user ID
+ */
+public class WebGet {
+    
+}
